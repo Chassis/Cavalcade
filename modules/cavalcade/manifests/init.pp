90b69db18e483d0c5c55fdf7fa64fc12a16e660a
Ryan McCue <me@ryanmccue.info>
2017-03-16 14:36:14 +1000

    Add systemd configuration for 16.04

--- modules/cavalcade/manifests/init.pp
+++ modules/cavalcade/manifests/init.pp
@@ -2,13 +2,21 @@ class cavalcade (
 	$path = "/vagrant/extensions/cavalcade",
 	$wproot = "/vagrant"
 ) {
-	file { "/etc/init/cavalcade.conf":
-		ensure => "file",
-		content => template("cavalcade/upstart.conf.erb"),
+	if versioncmp($::operatingsystemmajrelease, '15.04') >= 0 {
+		file { "/lib/systemd/system/cavalcade.service":
+			ensure => "file",
+			content => template("cavalcade/systemd.service.erb"),
+		}
+		File['/lib/systemd/system/cavalcade.service'] -> Service['cavalcade']
+	} else {
+		file { "/etc/init/cavalcade.conf":
+			ensure => "file",
+			content => template("cavalcade/upstart.conf.erb"),
+		}
+		File['/etc/init/cavalcade.conf'] -> Service['cavalcade']
 	}
 
 	service { "cavalcade":
 		ensure => "running",
-		require => [ File[ "/etc/init/cavalcade.conf" ] ],
 	}
 }
