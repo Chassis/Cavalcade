7aab9dcf399e62eaf6dba82e037e7c6f6190d20e
Stephen Edgar <stephen@netweb.com.au>
2017-03-21 21:05:09 +1100

    Puppet coding standards

--- modules/cavalcade/manifests/init.pp
+++ modules/cavalcade/manifests/init.pp
@@ -1,22 +1,23 @@
+# Cavalcade is a replacement for WordPress' built-in cron that runs as a daemon on your system
 class cavalcade (
-	$path = "/vagrant/extensions/cavalcade",
-	$wproot = "/vagrant"
+  $path = '/vagrant/extensions/cavalcade',
+  $wproot = '/vagrant'
 ) {
-	if versioncmp($::operatingsystemmajrelease, '15.04') >= 0 {
-		file { "/lib/systemd/system/cavalcade.service":
-			ensure => "file",
-			content => template("cavalcade/systemd.service.erb"),
-		}
-		File['/lib/systemd/system/cavalcade.service'] -> Service['cavalcade']
-	} else {
-		file { "/etc/init/cavalcade.conf":
-			ensure => "file",
-			content => template("cavalcade/upstart.conf.erb"),
-		}
-		File['/etc/init/cavalcade.conf'] -> Service['cavalcade']
-	}
+  if versioncmp($::operatingsystemmajrelease, '15.04') >= 0 {
+    file { '/lib/systemd/system/cavalcade.service':
+      ensure  => 'file',
+      content => template('cavalcade/systemd.service.erb'),
+    }
+    File['/lib/systemd/system/cavalcade.service'] -> Service['cavalcade']
+  } else {
+    file { '/etc/init/cavalcade.conf':
+      ensure  => 'file',
+      content => template('cavalcade/upstart.conf.erb'),
+    }
+    File['/etc/init/cavalcade.conf'] -> Service['cavalcade']
+  }
 
-	service { "cavalcade":
-		ensure => "running",
-	}
+  service { 'cavalcade':
+    ensure => 'running',
+  }
 }
